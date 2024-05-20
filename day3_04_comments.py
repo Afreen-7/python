ff6cd87099258d62a18f02714b4c8672761f495f
Afreen Tariq <136088665+Afreen-7@users.noreply.github.com>
2024-05-20 22:28:15 +0500

    Create  day3_04_comments.py

---
+++ day3_04_comments.py
@@ -0,0 +1,19 @@
+# subject of this python script
+# author by: Afreen tariq
+# where to conatct: github profile email 
+
+# step:1 why use single notation?
+print('afreen') 
+
+# step :2 when to you "" quotation marks?
+print("afreen")
+print("what's up?")
+
+# step:3 when to use '''triple quotation marks'''?
+print('''
+     afreen 
+     what's up
+     how are you? 
+    ''') # to write multiline string also use '' or "" quotation in it
+
+# when to use comment in python? mention 10 case study
