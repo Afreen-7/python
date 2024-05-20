2d72389af09ec288d5a93262dae878ad2050e61f
Afreen Tariq <136088665+Afreen-7@users.noreply.github.com>
2024-05-20 22:29:07 +0500

    Create  day3_05_varaibles.py

---
+++ day3_05_varaibles.py
@@ -0,0 +1,70 @@
+# varaibles: object containing specific values
+x=5 #numerical or integer varaible
+print(x)
+
+y=("we are learning python")
+print(y)
+
+x=15
+print(x)
+
+x=10+5
+print(x)
+
+#types/ class of varaible
+type(x)
+print(type(x))
+
+print(type(y))
+
+
+# rules to assign a varaibles
+# 1. should conatin letters, numbers, underscore
+# 2. do not start with numbers
+# 3. spaces are not allowed
+# 4. do not use functional keyword(median, break, test, mean)
+# 5. short and descriptive varaible Name
+# 6. lower and upercase letter sensitivity(prefer to use lower case)
+
+
+# fruit_basket="mangoes, oranges"
+# print(fruit_basket)
+
+# fruit_basket=8
+# fruit_basket="mangoes"
+# del fruit_basket
+# print(type(fruit_basket))
+# print(fruit_basket)
+
+# pros
+# it reduces the human effort for coding
+# easy to write for long database
+# easy to recall
+
+# strategy
+# only those names that are not reserv
+# don't ever write varaible name wth space instead use_
+# don't capitilize it
+# always use short varaible(max 2)
+# don't start with special character
+# don't use number in the begning
+# meaningful varible use
+# use goble trend (df=dataframe)
+# don't ever repeat the same varaible(first one will update)
+# don't use operator in varaible
+# snack update to snack_1, snack_2
+# don't use "" when using varaible
+
+name_2= "my name"
+fruit_basket=("mangoes","oranges", "apple", "banana")
+print(name_2)
+print(fruit_basket)
+
+print(type(name_2))
+print(type(fruit_basket))
+
+x="15"
+print(type(x))
+
+x=int(x)
+print(type(x))
