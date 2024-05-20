629d0385ac37b0351546d0aa780d1bfbd85526f4
Afreen Tariq <136088665+Afreen-7@users.noreply.github.com>
2024-05-20 22:27:25 +0500

    Create  day3_03_string.py

---
+++ day3_03_string.py
@@ -0,0 +1,19 @@
+print("hello world")
+print('learning python')
+print("i am 'learning python'")
+
+print('''
+    i am  afreen.
+    i am learning something that is important for me.
+    i still motivated that i can do it.
+    ''')
+print("what's up?")
+#``(name as back ticks) (we use them wen we write markdown file and show code separetely)
+
+print("2+3")
+# Assignment: what is type casting? and how to we use in python?
+
+
+# boolen data
+True
+False
