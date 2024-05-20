5448a3997162b182b999edb40c4bc431d7f17683
Afreen Tariq <136088665+Afreen-7@users.noreply.github.com>
2024-05-20 22:26:29 +0500

    Create day3_02_operator.py

---
+++ day3_02_operator.py
@@ -0,0 +1,14 @@
+print(2+4)
+print(5-6)
+print(7*8)
+print(2/8) #get float no.
+print(7%3) #remainder
+print(6//3) #get answer in int
+print(2**4) #power insted od using ^ this we use**
+
+
+# pemdas rule
+
+print(3**2/2*3/3+6-4/(2*6))
+
+# print(2+3         -5  )
